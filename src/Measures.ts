94a441b217a17e18fd8deff8540fbff234161006
Klaus Eckelt <Klaus.Eckelt@jku.at>
2018-10-10 19:39:39 +0200

    Add Calculation of Jaccard, Overlap and t-test

--- src/Measures.ts
+++ src/Measures.ts
@@ -1,6 +1,6 @@
 import {IAttributeDesc, Comparison, SCOPE, ISimilarityClass, ISetSimilarityClass, IMeasureOptions, Type} from './interfaces';
 import {defaultMeasureOptions} from './config';
-
+import * as d3 from 'd3';
 
 export const registeredClasses = new Array<ASimilarityClass>();
 export function MeasureDecorator() {
@@ -47,7 +47,23 @@ export class JaccardSimilarity extends ASimilarityClass implements ISetSimilarit
 
 
   calc(setA: Array<any>, setB: Array<any>) {
-    return 1 - Math.random(); // ]0,1]
+    let intersection = [];
+    const filteredsetA = setA.filter((itemA) => {
+      const indexB = setB.findIndex((itemB) => itemB === itemA); // check if there is a corresponding entry in the setB
+      if (indexB >= 0) {
+        intersection.push(itemA);
+        setB.splice(indexB, 1);
+        return false; // selItem will drop out of setA
+      }
+      return true;
+    });
+
+    // const intersection = selectionSet.filter(item => categorySet.indexOf(item) >= 0); // filter elements not in the second array
+    // const union = selectionSet.concat(categorySet).sort().filter((item, i, arr) => !i || item != arr[i-1]) // !i => if first elemnt, or if unqueal to previous item (item != arr[i-1]) include in arr
+
+    const score = intersection.length / (intersection.length + filteredsetA.length + setB.length);
+
+    return score || 0;
   }
 }
 
@@ -69,7 +85,22 @@ export class OverlapSimilarity extends ASimilarityClass implements ISetSimilarit
 
 
   calc(setA: Array<any>, setB: Array<any>) {
-    return 1 - Math.random(); // ]0,1]
+    const minSize = Math.min(setA.length, setB.length);
+
+    let intersection = [];
+    setA.filter((selItem) => {
+      const indexB = setB.findIndex((catItem) => catItem === selItem);
+      if (indexB >= 0) {
+        intersection.push(selItem);
+        setB.splice(indexB, 1);
+        return false; // selItem will drop out of selectionSet
+      }
+      return true;
+    });
+
+    const score = intersection.length / minSize;
+
+    return score || 0;
   }
 }
 
@@ -85,13 +116,26 @@ export class StudentTTest extends ASimilarityClass implements ISetSimilarityClas
     this.label = "Student's t-test";
     this.description = "Compares the means of two samples (assuimg equal variances in their respective normal distributions).";
 
-    this.type = Comparison.get(Type.NUMERICAL, Type.NUMERICAL);
+    this.type = Comparison.get(Type.CATEGORICAL, Type.NUMERICAL);
     this.scope = SCOPE.SETS;
   }
 
 
   calc(setA: Array<any>, setB: Array<any>) {
-    return 1 - Math.random(); // ]0,1]
+    const nSelection = setA.length;
+    const muSelection = d3.mean(setA);
+    const varSelection = d3.variance(setA);
+
+    //category
+    const nCategory = setB.length;
+    const muCategory = d3.mean(setB);
+    const varCategory = d3.variance(setB);
+
+    let scoreP1 = Math.sqrt((nSelection * nCategory * (nSelection + nCategory - 2)) / (nSelection + nCategory));
+    let scoreP2 = (muSelection - muCategory) / Math.sqrt((nSelection - 1) * varSelection + (nCategory - 1) * varCategory);
+    let score = scoreP1 * scoreP2;
+
+    return score || 0;
   }
 }
 
