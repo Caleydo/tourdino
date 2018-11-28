8df2cb61c5dfb06d74f57dc4ad01bad18943cc05
PatrickAdelberger <patrick.adelberger@jku.at>
2018-11-28 10:23:54 +0100

    added all visualisations

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export * from './Measures';
 export * from './util';
 export * from './measure_visualization/ParallelSets';
 export * from './measure_visualization/BoxPlot';
+export * from './measure_visualization/ScatterPlot';
+export * from './measure_visualization/LineChart';
