2c32fb1b244022a25061af53b2739d9d36321a08
PatrickAdelberger <patrick.adelberger@jku.at>
2019-02-12 11:38:30 +0100

    chi-square: added p-Value and cramer's V score is used for the score value; Caleydo/tdp_bi_bioinfodb#972

--- src/Measures.ts
+++ src/Measures.ts
@@ -66,53 +66,57 @@ export class ChiSquareTest extends ASimilarityMeasure {
     const setBCategories = setB.filter((item, index, self) => self.indexOf(item) === index);
     const allCategories = setACategories.concat(setBCategories).filter((item, index, self) => self.indexOf(item) === index);
 
-    const setASize = setA.length;
-    const setBSize = setB.length;
-    const overallSize = setASize+setBSize;
+    let score = 0;
+    let pValue = -1;
 
-    const table = [];
-    for(const currCat of allCategories) {
-      const amountSetA = setA.filter((item) => (item===currCat)).length;
-      const amountSetB = setB.filter((item) => (item===currCat)).length;
-      const sum = amountSetA+amountSetB;
-      const setAExp = (setASize * sum) / overallSize;
-      const setAChi = Math.pow(amountSetA - setAExp,2)/setAExp;
+    if (allCategories.length > 1) {
+      const setASize = setA.length;
+      const setBSize = setB.length;
+      const overallSize = setASize+setBSize;
 
-      const setBExp = (setBSize * sum) / overallSize;
-      const setBChi = Math.pow(amountSetB - setBExp,2)/setBExp;
-      const sumChi = setAChi + setBChi;
-      const currCell = {
-        category: currCat,
-        setA: amountSetA,
-        setAExp,
-        setAChi,
-        setB: amountSetB,
-        setBExp,
-        setBChi,
-        sum,
-        sumChi
-      };
-      table.push(currCell);
+      const table = [];
+      for(const currCat of allCategories) {
+        const amountSetA = setA.filter((item) => (item===currCat)).length;
+        const amountSetB = setB.filter((item) => (item===currCat)).length;
+        const sum = amountSetA+amountSetB;
+        const setAExp = (setASize * sum) / overallSize;
+        const setAChi = Math.pow(amountSetA - setAExp,2)/setAExp;
+
+        const setBExp = (setBSize * sum) / overallSize;
+        const setBChi = Math.pow(amountSetB - setBExp,2)/setBExp;
+        const sumChi = setAChi + setBChi;
+        const currCell = {
+          category: currCat,
+          setA: amountSetA,
+          setAExp,
+          setAChi,
+          setB: amountSetB,
+          setBExp,
+          setBChi,
+          sum,
+          sumChi
+        };
+        table.push(currCell);
+      }
+
+
+      const allChiForCategories = table.map((a) => (a.sumChi));
+      const chiSquare = allChiForCategories.reduce(this.getSum);
+      const rows = 2; // the two sets
+      const columns = table.length; //number of categories in the two sets
+      const df = (rows-1)*(columns-1);
+      // Phi
+      // const phi = Math.sqrt(chiSquare/overallSize);
+      // Cramer's V
+      const t = Math.min(rows-1,columns-1);
+      const cramerV = Math.sqrt(chiSquare/(overallSize*t));
+
+      score = cramerV;
+      pValue = 1- jStat.jStat.chisquare.cdf(chiSquare,df);
+      // console.log('ChiSquare - table: ', {setA,setB,allCategories,table,chiSquare,df,pValue});
     }
 
-
-    const allChiForCategories = table.map((a) => (a.sumChi));
-    const chiSquare = allChiForCategories.reduce(this.getSum);
-    const rows = 2; // the two sets
-    const columns = table.length; //number of categories in the two sets
-    const df = (rows-1)*(columns-1);
-    // Phi
-    const phi = Math.sqrt(chiSquare/overallSize);
-    // Cramer's V
-    const t = Math.min(rows-1,columns-1);
-    const cramerV = Math.sqrt(chiSquare/(overallSize*t));
-
-    console.log('ChiSquare - table: ', {setA,setB,allCategories,table,chiSquare,phi,cramerV});
-    let score = chiSquare;
-
-    score = score || 0;
-
-    return measureResultObj(score, -1);
+    return measureResultObj(score, pValue);
   }
 
   public getSum(total: number, numb: number) {
