6a46562f5484fbb84d4a0ab94a0ef67ababbeabc
Klaus Eckelt <Klaus.Eckelt@jku.at>
2019-04-04 12:25:49 +0200

    Jaccard: Return no p-Value if randomization is not possible Caleydo/tdp_bi_bioinfodb#904

--- src/Workers/JaccardRandom.ts
+++ src/Workers/JaccardRandom.ts
@@ -21,7 +21,7 @@ ctx.onmessage = (event) => {
       ctx.postMessage({score: actualScore, p: 0.0}); // Jaccard score is maximum, the other random sets can't get a higher score
     } else if (actualScore === 0) {
       ctx.postMessage({score: actualScore, p: 1.0}); // Jaccard score is minimum, the other random sets can't get a lower score
-    } else {
+    } else if (allData) { // check if allData is defined
       // The score is neither maximum nor minimum, so we compare it to random scores:
       const rndScores = new Array(1000); // array with 1000 entries
       const drawSize = setA.length;
@@ -34,6 +34,8 @@ ctx.onmessage = (event) => {
 
       const p = rndScores.filter((rndScore) => rndScore > actualScore).length/1000.0; //  filter the array so only higher jaccard scores remain, then divide by number of computations. .0 to force floating point division
       ctx.postMessage({score: actualScore, p});
+    } else {
+      ctx.postMessage({score: actualScore, p: -1});
     }
   } catch(error) {
     console.error(`Cannot calculate Jaccard p-value.\tError Type: ${error.name}\tMessage: ${error.message}\nStackTrace: ${error.stack}`);
