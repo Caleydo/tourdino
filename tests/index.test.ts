6d238d4c5fa1eac9a0f184fa1a591a0de4dec936
Klaus Eckelt <Klaus.Eckelt@jku.at>
2019-04-04 12:23:56 +0200

    Move precision to index test file Caleydo/tdp_bi_bioinfodb#904

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,12 @@
 /// <reference types="jasmine" />
-// import {hello} from '../src/index';
 
-// describe('index', () => {
-//   it('hello', () => {
-//     expect(hello()).toEqual('Hello World');
-//     expect(hello({})).toEqual('Hello World');
-//     expect(hello({ name: 'Test' })).toEqual('Hello Test');
-//   });
-// });
+export const PRECISION = 6; // difference of values must be <= 0,0000005 (10e-6/2)
+
+describe('Jasmine Test', () => {
+  it('close to', () => {
+    expect(0.5).toBeCloseTo(0.5+ 0.0000005, 6);
+    expect(0.5).not.toBeCloseTo(0.5+ 0.0000006, 6);
+    expect(6.5).toBeCloseTo(6.5 + 0.0000005, 6);
+    expect(6.5).not.toBeCloseTo(6.5 + 0.0000006, 6);
+ });
+});
