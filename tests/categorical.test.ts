4c4814307d317cb11add0413f447a46cdd1cf221
Klaus Eckelt <Klaus.Eckelt@jku.at>
2019-04-04 12:29:04 +0200

    Add adjusted Rand tests Caleydo/tdp_bi_bioinfodb#904

--- tests/categorical.test.ts
+++ tests/categorical.test.ts
@@ -59,3 +59,64 @@ describe('Jaccard index', () => {
   // TODO test randomization
 });
 
+
+
+describe('Adjusted Rand', () => {
+  let adjRand;
+
+  beforeAll(function() {
+    adjRand = new AdjustedRandIndex();
+  });
+
+  it('Perfect Match', async () => {
+    let ari = await adjRand.calc(['a', 'a', 'a', 'a'], ['a', 'a', 'a', 'a']);
+    expect(ari.scoreValue).toEqual(1);
+    expect(ari.pValue).toEqual(0);
+    ari = await adjRand.calc(['a', 'a', 'a', 'b'], ['e', 'e', 'e', 'f']);
+    expect(ari.scoreValue).toEqual(1);
+    expect(ari.pValue).toEqual(0);
+    ari = await adjRand.calc(['a', 'a', 'a', 'a'], ['b', 'b', 'b', 'b']);
+    expect(ari.scoreValue).toEqual(1);
+    expect(ari.pValue).toEqual(0);
+  });
+
+  it('should throw on different set sizes', async() => {
+    const a = ['a', 'a', 'b'];
+    const b = ['a', 'b', 'b', 'c'];
+
+    let error = undefined;
+    try {
+      await adjRand.calc(a, b);
+    } catch(e) {
+      error = e;
+    }
+    expect(error).toBeDefined();
+
+    error = undefined;
+    try {
+      await adjRand.calc(b, a);
+    } catch(e) {
+      error = e;
+    }
+    expect(error).toBeDefined();
+  });
+
+  it('3/4 match', async () => {
+    let ari = await adjRand.calc(['a', 'a', 'b', 'c'], ['a', 'a', 'b', 'b']);
+    expect(ari.scoreValue).toBeCloseTo(0.5714285714285715, PRECISION);
+    ari = await adjRand.calc(['a', 'a', 'b', 'c'], ['a', 'a', 'c', 'c']);
+    expect(ari.scoreValue).toBeCloseTo(0.5714285714285715, PRECISION);
+    ari = await adjRand.calc(['a', 'a', 'c', 'b'], ['a', 'a', 'b', 'b']);
+    expect(ari.scoreValue).toBeCloseTo(0.5714285714285715, PRECISION);
+    ari = await adjRand.calc(['c', 'b', 'a', 'a'], ['a', 'a', 'b', 'b']);
+    expect(ari.scoreValue).toBeCloseTo(0.5714285714285715, PRECISION);
+  });
+
+  it('Total  Mismatch', async () => {
+    const ari = await adjRand.calc(['a', 'a', 'a'], ['b', 'c', 'd']);
+    expect(ari.scoreValue).toEqual(0);
+    expect(ari.pValue).toEqual(1);
+  });
+
+  // TODO test randomization
+});
