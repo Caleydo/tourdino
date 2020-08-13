81cb3838fdb5d3133e9342c9501d7132ef13277a
oltionchampari <oltion.champari@datavisyn.io>
2020-08-13 15:05:13 +0200

    Explicitly import d3-gruber-boxplot

--- src/app/TouringPanel.ts
+++ src/app/TouringPanel.ts
@@ -2,6 +2,7 @@ import '../scss/main.scss';
 import {RankingAdapter} from '../tasks/RankingAdapter';
 import * as d3 from 'd3';
 import 'd3.parsets/d3.parsets';
+import 'd3-grubert-boxplot/box';
 import {LocalDataProvider} from 'lineupjs';
 import {PanelTab} from 'tdp_core/dist/lineup/internal/panel/PanelTab';
 import {IPanelTabExtensionDesc} from 'tdp_core/dist/lineup/internal/LineUpPanelActions';
