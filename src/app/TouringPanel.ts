220f84d897596cfc6aa23103645647e80e9d9f62
Klaus <Klaus.Eckelt@jku.at>
2021-01-19 11:37:11 +0100

    Restore TouringPanel.ts

--- src/app/TouringPanel.ts
+++ src/app/TouringPanel.ts
@@ -1,6 +1,7 @@
 import {RankingAdapter} from '../tasks/RankingAdapter';
 import * as d3 from 'd3';
 import 'd3.parsets/d3.parsets';
+import 'd3-grubert-boxplot/box';
 import {LocalDataProvider} from 'lineupjs';
 import {PanelTab} from 'tdp_core/dist/lineup/internal/panel/PanelTab';
 import {IPanelTabExtensionDesc} from 'tdp_core/dist/lineup/internal/LineUpPanelActions';
